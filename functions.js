9f72eb689a6983ebe9cd94f591fce17ad1d20229
wmusial98 <wiktoria.musial@gmail.com>
2022-09-16 12:41:16 -0700

    Write arithmetic functions and tests.

--- functions.js
+++ functions.js
@@ -1,19 +1,31 @@
 const add = function(a,b) {
-    return a+b;
+    return (countDecimals(a) < (2 || undefined) && countDecimals(b) < (2 || undefined)) ? a+b : 'ERROR';
 };
 
-/* const subtract = function(a,b) {
-    return a-b;
+const subtract = function(a,b) {
+    return (countDecimals(a) < (2 || undefined) && countDecimals(b) < (2 || undefined)) ? a-b : 'ERROR';
 };
 
 const multiply = function(a,b) {
-    return a*b;
+    return (countDecimals(a) < (2 || undefined) && countDecimals(b) < (2 || undefined)) ? a*b : 'ERROR';
 };
 
 const divide = function(a,b) {
-    return a/b;
-}; */
+    if(b == 0) {
+        return 'ggwp';
+    } else {
+        result = (countDecimals(a) < (2 || undefined) && countDecimals(b) < (2 || undefined)) ? a/b : 'ERROR';
+        return Math.round(result * 1000) / 1000;
+    }
+};
+
+let countDecimals = function(value) {
+    return (Math.floor(value) === value) ? 0 : value.toString().split('.')[1].length || 0;
+}
 
 module.exports = {
     add,
+    subtract,
+    multiply,
+    divide,
 };
--- functions.spec.js
+++ functions.spec.js
@@ -1,3 +1,7 @@
+/* const { expect } = require('@jest/globals');
+const { default: test } = require('node:test');
+const { describe } = require('yargs'); */
+const { expect } = require('@jest/globals');
 const functions = require('./functions');
 
 describe('add', () => {
@@ -7,10 +11,58 @@ describe('add', () => {
     test('works with negative numbers.', () => {
         expect(functions.add(3,-3)).toBe(0);
     });
-    test.skip('works with decimals.', () => {
+    test('works with decimals.', () => {
         expect(functions.add(1.5,19.8)).toBe(21.3);
     });
-    test.skip('rejects decimals longer than 1.', () => {
+    test('rejects long decimals', () => {
         expect(functions.add(5.887322,25)).toBe('ERROR');
+    });
+});
+describe('subtract', () => {
+    test('subtracts two integers.', () => {
+        expect(functions.subtract(27,9)).toBe(18);
+    });
+    test('works with negative sum.', () => {
+        expect(functions.subtract(9,27)).toBe(-18);
+    });
+    test('works with decimals.', () => {
+        expect(functions.subtract(10.5,4.3)).toBe(6.2);
+    });
+    test('rejects long decimals.', () => {
+        expect(functions.subtract(27,9.8444365)).toBe('ERROR');
+    });
+});
+describe('multiply', () => {
+    test('multiplies two integers.', () => {
+        expect(functions.multiply(3,55)).toBe(165);
+    });
+    test('multiplies negative integers.', () => {
+        expect(functions.multiply(9,-27)).toBe(-243);
+    });
+    test('multiplies two negative integers.', () => {
+        expect(functions.multiply(-10,-4)).toBe(40);
+    });
+    test('works with decimals.', () => {
+        expect(functions.multiply(2.8,9.6)).toBe(26.88);
+    });
+    test('rejects long decimals', () => {
+        expect(functions.multiply(1,35.33568)).toBe('ERROR');
+    });
+});
+describe('divide', () => {
+    test('divides two integers.', () => {
+        expect(functions.divide(5,5)).toBe(1);
+    });
+    test('divides negative integers.', () => {
+        expect(functions.divide(100,-25)).toBe(-4);
+    });
+    test('rounds imperfect fractions to 3 decimal points.', () => {
+        expect(functions.divide(22,7)).toBe(3.143);
+    });
+    test('divides by zero.', () => {
+        expect(functions.divide(5,0)).toBe('ggwp');
+    });
+    test('divides zero.', () => {
+        expect(functions.divide(0,12)).toBe(0);
     });
 });
